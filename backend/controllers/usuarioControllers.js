efb0dd01112949ef88b223c0fcf6c625a40352da
Federico <fedee.unates.2001@gmail.com>
2023-12-01 01:07:01 -0300

    Autenticación del usuario terminada

--- backend/controllers/usuarioControllers.js
+++ backend/controllers/usuarioControllers.js
@@ -20,8 +20,31 @@ const registrar = async (req, res) => {
   }
 };
 
-const autenticar = async () => {
-  res.json({ msg: "Autenticando usuario" });
+const autenticar = async (req, res) => {
+  const { email, password } = req.body;
+
+  //Comprobar si el usuario existe
+  const usuario = await Usuario.findOne({ email: email });
+  if (!usuario) {
+    const error = new Error("El usuario no existe");
+    return res.status(404).json({ msg: error.message });
+  }
+  //Comprobar si el usuario está confirmado
+  if (!usuario.confirmado) {
+    const error = new Error("Tu cuenta no a sido confirmada");
+    return res.status(403).json({ msg: error.message });
+  }
+  //Comprobar su password
+  if (await usuario.comprobarPassword(password)) {
+    res.json({
+      _id: usuario._id,
+      nombre: usuario.nombre,
+      email: usuario.email,
+    });
+  } else {
+    const error = new Error("Contraseña Incorrecta");
+    return res.status(403).json({ msg: error.message });
+  }
 };
 
 export { registrar, autenticar };
