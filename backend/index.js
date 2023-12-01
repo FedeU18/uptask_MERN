357a92f102c1eabf158ce51c7ff0870bac6414b4
Federico <fedee.unates.2001@gmail.com>
2023-12-01 00:20:05 -0300

    controlador para crear usuario empezado

--- backend/index.js
+++ backend/index.js
@@ -4,17 +4,18 @@ import dotenv from "dotenv";
 import usuarioRoutes from "./routes/usuarioRoute.js";
 
 const app = express();
+app.use(express.json());
 dotenv.config();
 conectarDB();
 
 //Routing
 app.get("/", (req, res) => {
-  res.send("<a href='/api/usuarios'>holis</a>");
+  res.send("<a href='/api/usuarios'>Usuarios</a>");
 });
 app.use("/api/usuarios", usuarioRoutes);
 
+//Servidor
 const PORT = process.env.PORT || 4000;
-
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
